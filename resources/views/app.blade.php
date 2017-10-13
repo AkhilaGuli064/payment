7b4ed3fff36befeba19ab6905af37755277c49a8
AkhilaGuli064 <akhilaguli@unomaha.edu>
2017-10-12 20:55:41 -0500

    added a space

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8"> 
     <title>Profile Management</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 </head>
